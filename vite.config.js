0a4d06e2afbe0df9c06dcec14a0a38eef403a65d
nousantx <nousantx@gmail.com>
2025-04-01 08:02:16 +0700

    feat(gh-pages): re-generate build ready page

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import anyframe from './.plugin/anyframe.js'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), anyframe()]
-})
